e0702f3986ff20ef9aeaffe78bec72af59ce450e
taniya-das <taniyatd30@gmail.com>
2025-03-10 18:10:45 +0100

    correct field names

--- src/database/model/resource_bundle/resource_bundle.py
+++ src/database/model/resource_bundle/resource_bundle.py
@@ -26,24 +26,24 @@ class ResourceBundle(ResourceBundleBase, AIResource, table=True):  # type: ignor
     __tablename__ = "resource_bundle"
 
     # Many-to-Many relationship linking ResourceBundle to external resources (URLs)
-    includes_external_resource: List[ExternalResource] = Relationship(
+    includes_external_reference: List[ExternalResource] = Relationship(
         link_model=many_to_many_link_factory("resource_bundle", ExternalResource.__tablename__)
     )
 
     # A list of AIResources that form part of this bundle
-    includes_resources: List[AIResourceORM] = Relationship(
+    includes_resource: List[AIResourceORM] = Relationship(
         link_model=many_to_many_link_factory("resource_bundle", AIResourceORM.__tablename__)
     )
 
     class RelationshipConfig(AIResource.RelationshipConfig):
-        includes_external_resource: List[str] = ManyToMany(
+        includes_external_reference: List[str] = ManyToMany(
             description="External resources (URLs) not in AIoD.",
             _serializer=AttributeSerializer("name"),
             deserializer=FindByNameDeserializerList(ExternalResource),
             default_factory_pydantic=list,
         )
 
-        includes_resources: List[int] = ManyToMany(
+        includes_resource: List[int] = ManyToMany(
             description="AIResources included in this bundle.",
             _serializer=AttributeSerializer("identifier"),
             deserializer=FindByIdentifierDeserializerList(AIResourceORM),
