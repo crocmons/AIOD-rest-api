6811d9ae4d372d8d057d76a2a4bb6398b30214bb
PGijsbers <p.gijsbers@tue.nl>
2025-04-08 17:13:12 +0200

    Return aiod entries for assets

--- src/routers/user_router.py
+++ src/routers/user_router.py
@@ -1,9 +1,14 @@
 from typing import Sequence
 
 from fastapi import APIRouter, Depends
+from sqlalchemy import select
+from sqlmodel import Session
 
 from authentication import KeycloakUser, get_user_or_raise
+from database.authorization import Permission
 from database.model.concept.concept import AIoDConcept
+from database.session import get_session
+from database.model.concept.aiod_entry import AIoDEntryORM, AIoDEntryRead
 
 
 def create(url_prefix: str) -> APIRouter:
@@ -14,16 +19,24 @@ def create(url_prefix: str) -> APIRouter:
         f"{url_prefix}/user/resources/{version}",
         tags=["User"],
         description="Return all your assets",
-        response_model=None,  # Look at review return stuff
+        response_model=list[AIoDEntryRead],  # TODO: Return AIoD Concept instead
     )(get_resources_for_logged_in_user)
     return router
 
 
 def get_resources_for_logged_in_user(
     user: KeycloakUser = Depends(get_user_or_raise),
-) -> Sequence[AIoDConcept]:
-    return _get_resources_for_user(user)
+    session: Session = Depends(get_session),
+) -> list[AIoDEntryORM]:
+    return _get_resources_for_user(user, session)
 
 
-def _get_resources_for_user(user: KeycloakUser) -> Sequence[AIoDConcept]:
-    return []
+def _get_resources_for_user(user: KeycloakUser, session: Session) -> list[AIoDEntryORM]:
+    # Get all resources for which the user has administration permissions
+    stmt = (
+        select(AIoDEntryORM)
+        .join(Permission.aiod_entry)
+        .where(Permission.user_identifier == user._subject_identifier)
+    )
+    entries = session.scalars(stmt).all()
+    return entries
--- src/tests/test_user_endpoints.py
+++ src/tests/test_user_endpoints.py
@@ -18,22 +18,20 @@ def test_my_resources_can_be_empty(client: TestClient) -> None:
     assert response.json() == [], "A user with no resources should get an empty list"
 
 
-@pytest.mark.skip()
 def test_my_resources_shows_draft_assets(client: TestClient, publication: Publication) -> None:
     register_asset(publication, owner=ALICE, status=EntryStatus.DRAFT)
     with logged_in_user(ALICE):
         response = client.get("/user/resources/v1", headers={"Authorization": "fake token"})
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == [], "Draft assets should be included in this view."
+    assert len(response.json()) == 1, "Draft assets should be included in this view."
 
 
-@pytest.mark.skip()
 def test_my_resources_shows_published_assets(client: TestClient, publication: Publication) -> None:
     register_asset(publication, owner=ALICE, status=EntryStatus.PUBLISHED)
     with logged_in_user(ALICE):
         response = client.get("/user/resources/v1", headers={"Authorization": "fake token"})
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == [], "Published assets should be included in this view."
+    assert len(response.json()) == 1, "Published assets should be included in this view."
 
 
 @pytest.mark.skip()
@@ -47,7 +45,6 @@ def test_my_resources_shows_mixed_assets(client: TestClient, publication: Public
     assert True, "The overview should contain attributes specific to each asset"
 
 
-@pytest.mark.skip()
 def test_my_resources_shows_only_own_resources(client: TestClient, publication_factory: Callable[[], Publication]) -> None:
     register_asset(publication_factory(), owner=ALICE, status=EntryStatus.DRAFT)
     register_asset(publication_factory(), owner=ALICE, status=EntryStatus.PUBLISHED)
