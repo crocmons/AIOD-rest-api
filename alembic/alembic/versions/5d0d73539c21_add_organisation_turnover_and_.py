95d91d941578cf9679940806267f23935fb7a5bc
taniya-das <taniyatd30@gmail.com>
2025-05-12 17:20:41 +0200

    ruff

--- alembic/alembic/versions/5d0d73539c21_add_organisation_turnover_and_.py
+++ alembic/alembic/versions/5d0d73539c21_add_organisation_turnover_and_.py
@@ -5,6 +5,7 @@ Revises: 1662d64ebe23
 Create Date: 2025-05-12 14:00:35.644646
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,15 +13,40 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '5d0d73539c21'
-down_revision: Union[str, None] = '1662d64ebe23'
+revision: str = "5d0d73539c21"
+down_revision: Union[str, None] = "1662d64ebe23"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
-def upgrade() -> None:
-    pass
+def upgrade():
+    op.add_column("organisation", sa.Column("turnover_identifier", sa.Integer(), nullable=True))
+    op.add_column(
+        "organisation", sa.Column("number_of_employees_identifier", sa.Integer(), nullable=True)
+    )
+
+    op.create_foreign_key(
+        "fk_organisation_turnover_identifier",
+        "organisation",
+        "company_revenue",
+        ["turnover_identifier"],
+        ["identifier"],
+    )
+
+    op.create_foreign_key(
+        "fk_organisation_number_of_employees_identifier",
+        "organisation",
+        "company_revenue",
+        ["number_of_employees_identifier"],
+        ["identifier"],
+    )
 
 
 def downgrade() -> None:
-    pass
+    op.drop_constraint(
+        "fk_organisation_number_of_employees_identifier", "organisation", type_="foreignkey"
+    )
+    op.drop_constraint("fk_organisation_turnover_identifier", "organisation", type_="foreignkey")
+
+    op.drop_column("organisation", "number_of_employees_identifier")
+    op.drop_column("organisation", "turnover_identifier")
