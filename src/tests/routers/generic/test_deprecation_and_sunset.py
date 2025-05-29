f8b6bafda81249679f77638b036d0506aa19249e
PGijsbers <p.gijsbers@tue.nl>
2025-05-29 15:28:55 +0200

    Ensure the latest versions do not issue warnings

--- src/tests/routers/generic/test_deprecation_and_sunset.py
+++ src/tests/routers/generic/test_deprecation_and_sunset.py
@@ -11,3 +11,10 @@ def test_version_1(client: TestClient):
     assert 'Deprecation' in response.headers
     assert 'Sunset' in response.headers
     assert 'Link' in response.headers
+
+
+def test_nondeprecated_versions(client: TestClient):
+    response = client.get("/datasets")
+    assert response.status_code == HTTPStatus.OK
+    assert 'Deprecation' not in response.headers
+    assert 'Sunset' not in response.headers
