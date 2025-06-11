6447c590b44817a7fbab4b5ae1af5621df2f0a4e
PGijsbers <p.gijsbers@tue.nl>
2025-06-11 14:42:07 +0200

    Add logging, bugfixes

--- alembic/alembic/versions/8b054cdc9261_synchronize_identifiers.py
+++ alembic/alembic/versions/8b054cdc9261_synchronize_identifiers.py
@@ -5,7 +5,7 @@ Revises: 1662d64ebe23
 Create Date: 2025-05-14 09:07:47.064937
 
 """
-
+import logging
 # no user input
 # ruff: noqa: S608
 from typing import Sequence, Union, NamedTuple
@@ -24,6 +24,8 @@ revision: str = "8b054cdc9261"
 down_revision: Union[str, None] = "1662d64ebe23"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
+
+logger = logging.getLogger("alembic")
 
 
 class ParentTable(NamedTuple):
@@ -117,6 +119,7 @@ def upgrade() -> None:
     # (e.g., /dataset/1). For parent identifiers we store it all together for parent routers.
     # TODO: Actually generate new identifiers
     for child in aiod_concept.children:
+        logger.info(f"Creating conversion table for {child}")
         map_table = f"_{child}_identifier_map"
         op.create_table(
             map_table,
@@ -128,6 +131,7 @@ def upgrade() -> None:
         )
 
     for parent in [ai_resource, ai_asset, agent]:
+        logger.info(f"Creating conversion table for {parent.name}")
         map_table = f"_{parent.name}_identifier_map"
         op.create_table(
             map_table,
@@ -142,6 +146,7 @@ def upgrade() -> None:
 
     # Now we can continue with data migration
     # First we delete a conflicting CHECK constraint: https://github.com/aiondemand/AIOD-rest-api/issues/518
+    logger.info("Dropping contact CHECK constraint.")
     op.execute(
         "ALTER TABLE contact DROP CONSTRAINT contact_person_and_organisation_not_both_filled"
     )
@@ -154,15 +159,18 @@ def upgrade() -> None:
             ai_resource.name,
             *aiod_concept.children,
         ]
+        logger.info("Fetching existing foreign key constraints.")
         constraints = session.execute(
             text(
-                "SELECT refs.CONSTRAINT_NAME, refs.DELETE_RULE, kcu.TABLE_NAME, kcu.COLUMN_NAME, kcu.REFERENCED_TABLE_NAME, kcu.REFERENCED_COLUMN_NAME"
+                "SELECT refs.CONSTRAINT_NAME, refs.DELETE_RULE, kcu.TABLE_NAME, kcu.COLUMN_NAME, kcu.REFERENCED_TABLE_NAME, kcu.REFERENCED_COLUMN_NAME "
                 "FROM information_schema.REFERENTIAL_CONSTRAINTS as refs "
                 "JOIN information_schema.KEY_COLUMN_USAGE as kcu "
                 "ON refs.CONSTRAINT_NAME=kcu.CONSTRAINT_NAME "
                 f"WHERE refs.REFERENCED_TABLE_NAME IN ({', '.join(map(repr, tables_with_referenced_key))});"
             )
         )
+    constraints = list(constraints)
+    logger.info(f"Dropping {len(constraints)} foreign key constraints.")
     for constraint, delete_rule, from_table, from_column, to_table, to_column in constraints:
         op.execute(f"ALTER TABLE {from_table} DROP FOREIGN KEY {constraint}")
 
@@ -170,10 +178,12 @@ def upgrade() -> None:
     for constraint, delete_rule, from_table, from_column, to_table, to_column in constraints:
         for table, column in [(to_table, to_column), (from_table, from_column)]:
             if (table, column) not in updated_columns:
+                logger.info(f"Altering {table}.{column} to VARCHAR(40).")
                 op.execute(f"ALTER TABLE {table} CHANGE COLUMN {column} {column} VARCHAR(40);")
                 updated_columns.add((table, column))
 
     for constraint, delete_rule, from_table, from_column, to_table, to_column in constraints:
+        logger.info(f"Adding back constraint {constraint}.")
         op.execute(
             f"ALTER TABLE {from_table} "
             f"ADD CONSTRAINT {constraint} "
@@ -188,15 +198,17 @@ def upgrade() -> None:
     # which leads to (temporary) duplicate keys. To avoid that, we add a temporary offset during
     # the update, and recover the original identifier afterwards.
     for table in aiod_concept.children:
+        logger.info(f"Assigning new identifiers to {table}.")
         op.execute(
-            f"UPDATE {table} SET identifier=CONCAT('{abbreviations[table]}', aiod_entry_identifier)"
+            f"UPDATE {table} SET identifier=CONCAT('{abbreviations[table]}', '_', aiod_entry_identifier)"
         )
 
     for table in [agent, ai_asset, ai_resource]:
         child_data = "UNION ".join(
-            f"SELECT {table.fk_identifier}, identifierFROM {child_table} "
+            f"SELECT {table.fk_identifier}, identifier FROM {child_table} "
             for child_table in table.children
         )
+        logger.info(f"Assigning new identifiers to {table.name}.")
         op.execute(
             f"UPDATE {table.name} "
             f"JOIN ({child_data}) as child "
