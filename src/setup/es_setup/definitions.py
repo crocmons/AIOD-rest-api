276a68b724955af11cdf6f94a059c2c60543935b
Pieter Gijsbers <p.gijsbers@tue.nl>
2025-06-20 23:38:39 +0200

    Update ES indexing for string identifierl

--- src/setup/es_setup/definitions.py
+++ src/setup/es_setup/definitions.py
@@ -2,7 +2,7 @@ BASE_MAPPING = {
     "mappings": {
         "properties": {
             "date_modified": {"type": "date"},
-            "identifier": {"type": "long"},
+            "identifier": {"type": "keyword"},
             "name": {"type": "text", "fields": {"keyword": {"type": "keyword"}}},
             "platform": {"type": "text", "fields": {"keyword": {"type": "keyword"}}},
             "description_plain": {"type": "text", "fields": {"keyword": {"type": "keyword"}}},
