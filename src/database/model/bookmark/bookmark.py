acd02db0d08004d73987515b2d344c78fee50786
taniya-das <taniyatd30@gmail.com>
2025-06-30 16:36:46 +0200

    fix format errors

--- src/database/model/bookmark/bookmark.py
+++ src/database/model/bookmark/bookmark.py
@@ -2,12 +2,13 @@ from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional
 from datetime import datetime
 
+
 class BookmarkBase(SQLModel):
-    user_identifier: str  
+    user_identifier: str
     resource_identifier: str
     created_at: datetime = Field(default_factory=datetime.utcnow)
-    
-class Bookmark(BookmarkBase, table=True): # type: ignore [call-arg]
+
+
+class Bookmark(BookmarkBase, table=True):  # type: ignore [call-arg]
     id: Optional[int] = Field(default=None, primary_key=True)
     __tablename__ = "bookmark"
-    
